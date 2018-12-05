bae7abc5658bc75927ef8643a42203af9b47a384
prestokp <prestokp@rose-hulman.edu>
2018-12-04 21:46:43 -0500

    It feels like I've been here for a month already...

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,21 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    for k in range(n):
+        nstart = rg.Point(point.x,point.y+(30*k))
+        nend = rg.Point(point.x+length,point.y+(30*k))
+        line = rg.Line(nstart,nend)
+        line.attach_to(window)
+        window.render()
+    return
+
+
+
+
+
+
+
+
 
 
 def run_test_draw_lines():
@@ -174,6 +189,13 @@ def draw_lines(n, point, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    for k in range(n):
+        start = rg.Point(point.x,point.y)
+        end = rg.Point(point.x+100, point.y-100+(200/(n-1)*k))
+        line = rg.Line(start,end)
+        line.attach_to(window)
+        window.render()
+    return
 
 
 # -----------------------------------------------------------------------------
